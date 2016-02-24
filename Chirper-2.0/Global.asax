6587c4dc960fc2b240acce1f031155a1302319bf
Mykyta Nikitenko <babun.babusha@gmail.com>
2016-02-24 18:41:41 +0200

    Created empty project

---
+++ Chirper-2.0/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Chirper_2._0.MvcApplication" Language="C#" %>
---
+++ Chirper-2.0/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Chirper_2._0.Startup))]
+namespace Chirper_2._0
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
